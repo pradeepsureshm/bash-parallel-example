91c615f474f8e4f8d7f7ecd3d13c90591e3cc2bd
pradeepsureshm <90941835+pradeepsureshm@users.noreply.github.com>
2022-09-19 14:59:07 -0700

    Update relyance-ai-sci-setup.sh

--- relyance-ai-sci-setup.sh
+++ relyance-ai-sci-setup.sh
@@ -2,6 +2,8 @@ sudo yum update -y
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-402.0.0-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-402.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh --quiet
+ls
+ls ~/google-cloud-sdk/bin
 export PATH=$PATH:~/google-cloud-sdk/bin
 
 sudo yum install docker -y
